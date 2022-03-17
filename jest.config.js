2767768829a85a5d075db8f5096fd55aacb59e71
Michael Morera <michael.morera92@gmail.com>
2022-03-17 01:31:54 -0600

    Created tests for API testing. Yaml CI file created too

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,8 @@
 // Sync object
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node"
 };
 
 module.exports = config;
-
-// Or async function
-module.exports = async () => {
-  return {
-    verbose: true,
-  };
-};
--- src/app.test.ts
+++ src/app.test.ts
@@ -1,5 +1,57 @@
-describe("app.test", () => {
-    test("initiate", () => {
-      expect("1").toEqual("1");
+import request from 'supertest';
+
+  const url = 'http://localhost:9999/api';
+
+describe('app.test', () => {
+    test('initiate', () => {
+      expect('1').toEqual('1');
     });
 });
+
+describe('Test Get Endpoint', () => {
+  it('Get Hit with num 1', async () => {
+    const res = await request(url)
+      .get('/foo/' + 1)
+    expect(res.statusCode).toEqual(501)
+  })
+  it('Get Hit with num 2', async () => {
+    const res = await request(url)
+      .get('/foo/' + 2)
+    expect(res.statusCode).toEqual(200)
+  })
+  it('Get Hit with num 4', async () => {
+    const res = await request(url)
+      .get('/foo/' + 4)
+    expect(res.statusCode).toEqual(200)
+  })
+  it('Get Hit with num 6', async () => {
+    const res = await request(url)
+      .get('/foo/' + 6)
+    expect(res.statusCode).toEqual(200)
+  })
+})
+
+describe('Test Post Endpoint', () => {
+  it('Post Hit with num 1', async () => {
+    const res = await request(url)
+      .post('/foo/').send({'id':1})
+    expect(res.statusCode).toEqual(200)
+    expect(res.text).toEqual('bar');
+  })
+  it('Post Hit with num 2', async () => {
+    const res = await request(url)
+      .post('/foo/').send({'id':2})
+    expect(res.statusCode).toEqual(200)
+    expect(res.text).toEqual('bass');
+  })
+  it('Post Hit with num 6', async () => {
+    const res = await request(url)
+      .post('/foo/').send({'id':6})
+    expect(res.statusCode).toEqual(200)
+  })
+  it('Post Hit with no num', async () => {
+    const res = await request(url)
+      .post('/foo/').send({'id':'this'})
+    expect(res.statusCode).toEqual(400)
+  })
+})
